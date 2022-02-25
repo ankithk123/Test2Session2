d4dae3159a779c8d09dc3894c0232c83d66c7f1f
Ankith  K <96900989+ankithk123@users.noreply.github.com>
2022-02-25 15:46:22 +0000

    p1final

--- p1final.c
+++ p1final.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+void input(float *x1,float *y1, float *x2, float *y2)
+{
+  printf("enter the x1 coordinate :");
+  scanf("%f",x1);
+  printf("enter the y1 coordinate:");
+  scanf("%f",y1);
+  printf("enter the x2 corodinate:");
+  scanf("%f",x2);
+  printf("enter the x2 corodinate:");
+  scanf("%f",y2);
+  
+  
+}
+void find_distance(float x1,float y1,float x2,float y2,float *area)
+{
+  area=sqrt(((x2-x1)(x2-x1))p1finals+((y2-y1)*(y2-y1)));
+  
+}
+void output(float x1,float y1,float x2,float y2,float area )
 
+{
+  printf("the difference between the two points is %0.3f",area);
+}
+int main()
+{
+  float x1,y1,x2,y2,area;
+  input(&x1,&y1,&x2,&y2);
+  find_distance(x1,y1,x2,y2,&area);
+  output(x1,y1,x2,y2,area);
+  return 0;
+}
--- p3final.c
+++ p3final.c
@@ -1,1 +1,46 @@
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("enter the number:");
+  scanf(""%d",&n);
+ return n;
+  
+}
+int is_prime(int n)
+{
+  int prime=0;
+  for(int i=2;i<=n/2;i++)
+    {
+if(n%i==0)
+{
+  prime=1;
+  break;
+}
+  }
+  return prime;
+}
+void output9int n,int prime)
+  {
+    if(n == 0 || n == 1)
+    {
+      printf("it is neither prime nor composite\n");
+    }
+    else if(prime==0)
+    {
+      printf("%d is a prime number\n",n);
+    }
+    else
+    {
+      printf("%d id a composite number\n",n);
+    }
+  }
+int main()
+{
+  int n,prime=0;
+  n= input_number();
+  prime =is_prime();
+  output(n,prime);
+  return 0;
+}
 
